3c8dd766dc8cfa92489d6eb65b89f7df43d5c309
Yakini <yakinilatha09@gmail.com>
2020-10-15 10:46:52 -0600

    fixed issue-cannot find module

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
-import AboutPage from "./pages/AboutPage";
-import PortfolioPage from "./pages/PortfolioPage";
-import ContactPage from "./pages/ContactPage";
 import Wrapper from "./components/Wrapper";
 import './style.css';
 
@@ -14,10 +11,6 @@ function App() {
           <div>
           <Wrapper>
           <Route exact path="/" component={IndexPage} />
-          {/* <Route exact path="/home" component={IndexPage} />
-          <Route exact path="/About" component={AboutPage} />
-          <Route exact path="/Portfolio" component={PortfolioPage} />
-          <Route exact path="/Contact" component={ContactPage} /> */}
           </Wrapper> 
           </div>
         
