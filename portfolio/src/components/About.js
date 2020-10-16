fd1f0aafd1508db96fc367b707db323b93660350
Yakini <yakinilatha09@gmail.com>
2020-10-16 10:48:00 -0600

    included the correct links

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -8,13 +8,14 @@ function About(){
    
     <div id="about">
 
-      <div className="container-fluid aboutus">
+      <div className="aboutus">
         <div className="row">
           <div className="col-sm-3 text-center">
             <img src={myImg} alt="" className="imgResponsive img-circle"></img>
           </div>
           <div className="col-sm-9 mt-5 mb-5">
           <h2 className="heading">About Me</h2>
+          <div>
             <p className="lead">
            
               Hi !!! My name is Yakini. Full Stack Web Developer with the knowledge to build responsive websites that focus mainly on rich user experience. With each passing day, I am gaining the knowledge in various technologies and tools needed to build a successful career as a full stack web developer. 
@@ -44,7 +45,7 @@ function About(){
               <li className="list-group-item">MySQL/MongoDB</li>
               <li className="list-group-item">Java</li>
             </ul>
-          
+          </div>
           </div>
         </div>
       </div>
