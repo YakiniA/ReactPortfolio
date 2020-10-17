00319e00ba98247cc40577d4ce88c0123379a652
Yakini <yakinilatha09@gmail.com>
2020-10-16 18:34:51 -0600

    Updates

---
+++ precache-manifest.804475533817f9c4553aa4cf2a54d465.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e607e48053d7a970d141064c0c4f75",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "090d1fba6b1b1ba0d3f4",
+    "url": "/ReactPortfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "02878cc95608cd1cd8d3",
+    "url": "/ReactPortfolio/static/css/main.e52506c4.chunk.css"
+  },
+  {
+    "revision": "090d1fba6b1b1ba0d3f4",
+    "url": "/ReactPortfolio/static/js/2.a66f399b.chunk.js"
+  },
+  {
+    "revision": "63bc195937561bf0cddd771a638bba66",
+    "url": "/ReactPortfolio/static/js/2.a66f399b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02878cc95608cd1cd8d3",
+    "url": "/ReactPortfolio/static/js/main.6cd47bec.chunk.js"
+  },
+  {
+    "revision": "97a1c7d0b592075495b7",
+    "url": "/ReactPortfolio/static/js/runtime-main.ad87311a.js"
+  },
+  {
+    "revision": "aa760cfd80c6a22ac91bee792cfd6239",
+    "url": "/ReactPortfolio/static/media/CodingQuizPortfolio.aa760cfd.jpg"
+  },
+  {
+    "revision": "a9416837e3eee833004e0c8d9350da94",
+    "url": "/ReactPortfolio/static/media/MyImage.a9416837.jpg"
+  },
+  {
+    "revision": "089f9f05ed861b0ecb5288cd459cb1a4",
+    "url": "/ReactPortfolio/static/media/ReadMeGenerator.089f9f05.jpg"
+  },
+  {
+    "revision": "9631bd7190cb86f12d40f04afe5f7da6",
+    "url": "/ReactPortfolio/static/media/Screenshot.9631bd71.jpg"
+  },
+  {
+    "revision": "dfb3d5520b6c1004c91e0ec24226762e",
+    "url": "/ReactPortfolio/static/media/Snapshot.dfb3d552.png"
+  },
+  {
+    "revision": "9263026cd34fd402cd4edf7c088f9923",
+    "url": "/ReactPortfolio/static/media/TeamGenerator.9263026c.gif"
+  },
+  {
+    "revision": "7ff5fd795001fe01b5fff18d0715018c",
+    "url": "/ReactPortfolio/static/media/WeatherDashboardportfolio.7ff5fd79.jpg"
+  },
+  {
+    "revision": "6a0ce0871142f7e260349b0096a212c8",
+    "url": "/ReactPortfolio/static/media/WorkdaySchedulerPortfolio.6a0ce087.jpg"
+  },
+  {
+    "revision": "bd821f8f589d1b83c3e0ba94d9b5fa60",
+    "url": "/ReactPortfolio/static/media/YARecent.bd821f8f.png"
+  },
+  {
+    "revision": "57df12ab3ddf84584d2aa83b061fc73c",
+    "url": "/ReactPortfolio/static/media/Yakini Resume.57df12ab.pdf"
+  },
+  {
+    "revision": "d4142f93827eb57b1cf678bb28831bd4",
+    "url": "/ReactPortfolio/static/media/bgimg6.d4142f93.jpg"
+  },
+  {
+    "revision": "dfbd5a272ce500e33780980dd5581868",
+    "url": "/ReactPortfolio/static/media/employee-tracker.dfbd5a27.gif"
+  },
+  {
+    "revision": "389dbf694e5e51dddf2d10999a0f7bea",
+    "url": "/ReactPortfolio/static/media/keyboardImg.389dbf69.jpg"
+  },
+  {
+    "revision": "b17134b4d64b6fe38ad4cb4e36544572",
+    "url": "/ReactPortfolio/static/media/portfolioBackground.b17134b4.jpg"
+  },
+  {
+    "revision": "1148f1dda006f12ee3ba1919be044aab",
+    "url": "/ReactPortfolio/static/media/spaceexplorationportfolio.1148f1dd.jpg"
+  },
+  {
+    "revision": "cd751b405b8b631489e0155a91a9aa5b",
+    "url": "/ReactPortfolio/static/media/welcomePage.cd751b40.png"
+  }
+]);
